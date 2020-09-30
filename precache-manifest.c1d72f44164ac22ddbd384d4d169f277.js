0563174f06e0124a369faacb558894012f52d6b5
lthong <appyegde58965@gmail.com>
2020-09-30 15:36:12 +0800

    Updates

---
+++ precache-manifest.c1d72f44164ac22ddbd384d4d169f277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5fbc4811e6d5b3b47603bd5c3801274",
+    "url": "/react-testing-example/index.html"
+  },
+  {
+    "revision": "6540b633ce7156b3662a",
+    "url": "/react-testing-example/static/css/main.62365ccb.chunk.css"
+  },
+  {
+    "revision": "4db9139cd992df00a502",
+    "url": "/react-testing-example/static/js/2.85648dee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-testing-example/static/js/2.85648dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6540b633ce7156b3662a",
+    "url": "/react-testing-example/static/js/main.15cd71eb.chunk.js"
+  },
+  {
+    "revision": "a7d5a91f0c639dd7bf49",
+    "url": "/react-testing-example/static/js/runtime-main.c8187c87.js"
+  }
+]);
